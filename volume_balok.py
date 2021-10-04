8c8893ad599eef4fdd1d427dfa6d7043b2078a10
farhanakml <akmaluntukkuliah@gmail.com>
2021-10-04 21:40:22 +0700

    volume_balok.py

---
+++ volume_balok.py
@@ -0,0 +1,12 @@
+# Nama : Muhammad Farhan Akmal
+# Username : farhanakml
+
+print("Menghitung Volume Balok")
+
+panjang = int(input("Masukkan Panjang : "))
+lebar = int(input("Masukkan Lebar : "))
+tinggi = int(input("Masukkan Tinggi : "))
+
+volume = panjang * lebar * tinggi
+
+print(f"Maka volume balok adalah {volume}")
