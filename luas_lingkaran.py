dc73745c0956ea7046587ae025a84f956f77e7c9
RolandoPranataAR <rolandopranata@gmail.com>
2021-10-04 22:33:23 +0700

    luas_lingkaran.py

---
+++ luas_lingkaran.py
@@ -0,0 +1,7 @@
+# Nama : Rolando Pranata
+# Username : rolandopranata
+
+phi = 3.14
+r = float(input("Masukkan panjang jari-jari lingkaran: "))
+luas = phi*r*r
+print("Hasil luas lingkaran adalah : "+ str(luas))
