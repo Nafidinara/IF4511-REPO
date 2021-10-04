6185ac151b909c050d6913992e90139ccee82bb7
dotnaonweh <hellonudes66@gmail.com>
2021-10-04 22:42:34 +0700

    menghitung volume tabung

---
+++ volume_tabung.py
@@ -0,0 +1,8 @@
+# menghitung volume tabung
+
+radius = float(input("Radius  > "))
+tinggi = float(input("Tinggi > "))
+radius2 = radius ** 2
+phi = 22/7
+v = phi * radius2 * tinggi
+print("Volume nya > ",int(v))
