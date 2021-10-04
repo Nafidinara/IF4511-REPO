c137c914a0e926daa0332e1ca62c05eab335d05e
Fawaz AR <53639263+fawazalrasyid@users.noreply.github.com>
2021-10-04 23:22:57 +0700

    Revert "luas_lingkaran.py"

---
+++ luas_lingkaran.py
@@ -0,0 +1,7 @@
+# Nama : Rolando Pranata
+# Username : rolandopranata
+
+phi = 3.14
+r = float(input("Masukkan panjang jari-jari lingkaran: "))
+luas = phi*r**2
+print("Hasil luas lingkaran adalah : ", luas)
