9e2eaa1f94df0287a746d297b156a8d6b76cbdc9
Fawaz AR <53639263+fawazalrasyid@users.noreply.github.com>
2021-10-04 16:15:28 +0700

    Add luas_segitiga.py

---
+++ luas_segitiga.py
@@ -0,0 +1,8 @@
+# Nama : Fawaz Al Rasyid
+# Username : fawazalrasyid
+
+alas = float(input("Masukkan alas segitiga: "))
+tinggi = float(input("Masukkan tinggi segitiga: "))
+
+luasSegitiga = 0.5 * alas * tinggi
+print("Luas segitiga:", luasSegitiga)
