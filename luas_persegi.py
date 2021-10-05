71ce2b1c85b00573417dc06fc859b102f0964716
Habieb Fathur Rahman <hydradev13@gmail.com>
2021-10-05 12:03:59 +0700

    Menghitung luas persegi

---
+++ luas_persegi.py
@@ -0,0 +1,10 @@
+# Nama : Habieb Fathur Rahman
+# Username : Iam-Hydra
+
+print("MENGHITUNG LUAS PERSEGI")
+
+s = float(input("\nMasukan Panjang Sisi: "))
+
+luas = s**2
+
+print("\nLuas Persegi \t\t:",luas)
