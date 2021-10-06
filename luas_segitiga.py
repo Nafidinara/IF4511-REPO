8ca0137527263e9a83c9a027e8303630423eac74
Muhammad Fahli Saputra <saputra@fahli.net>
2021-10-06 09:33:41 +0800

    menghitung luas segitiga

---
+++ luas_segitiga.py
@@ -0,0 +1,10 @@
+# Nama : Muhammad Fahli Saputra
+# Username : fahlisaputra
+
+print("MENGHITUNG LUAS SEGITIGA SAMA KAKI")
+
+a = float(input("\nMasukan Panjang Alas: "))
+t = float(input("\nMasukan Tinggi: "))
+luas = 1/2*a*t
+
+print("\nLuas Segitiga \t\t:",luas)
